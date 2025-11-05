b02101d4cd8a0a185b3c236ebc8f79ba2cc85fa5
Leo <leo.baudry@edu.univ-fcomte.fr>
2025-11-05 20:40:51 +0100

    test 9

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,6 +3,6 @@ import type { TypedPocketBase } from "./pocketbase-types";
 var path='';
 if(import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'    
-else path = 'https://sae301.leo-baudry.fr:443'    
+else path = 'http://localhost:6767'    
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
