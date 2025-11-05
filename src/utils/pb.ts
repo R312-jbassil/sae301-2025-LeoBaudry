cd9afafcecdaf865c4ce442b4ee1870941a233bf
Leo <leo.baudry@edu.univ-fcomte.fr>
2025-11-05 16:47:20 +0100

    commit deploy.yml

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,6 +1,8 @@
-import PocketBase from "pocketbase";
+import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
-
-const pb = new PocketBase("http://127.0.0.1:8090") as TypedPocketBase;
-
+var path='';
+if(import.meta.env.MODE === 'development')
+    path = 'http://localhost:8090'    
+else path = 'https://sae301.leo-baudry.fr:443'    
+const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
