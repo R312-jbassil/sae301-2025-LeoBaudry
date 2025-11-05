cf2087c693c05e10badbc05dd6f825fe6931d22f
Leo <leo.baudry@edu.univ-fcomte.fr>
2025-11-05 21:44:06 +0100

    ça a marché avant, mais maintenant ça va fonctionner encore mieux j'en suis certain

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,6 +3,6 @@ import type { TypedPocketBase } from "./pocketbase-types";
 var path='';
 if(import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'    
-else path = 'http://localhost:6767'    
+else path = 'http://127.0.0.1:6767'    
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
