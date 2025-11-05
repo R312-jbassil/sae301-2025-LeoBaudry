0116e64beeee5d83870e355d73cb641903f676d7
Leo <leo.baudry@edu.univ-fcomte.fr>
2025-11-05 21:48:53 +0100

    connexion et inscription marchent plus sur le site déployé... idem la nav du menu s'affiche pas (:

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,8 +1,14 @@
 import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
+
 var path='';
-if(import.meta.env.MODE === 'development')
-    path = 'http://localhost:8090'    
-else path = 'http://127.0.0.1:6767'    
+if(import.meta.env.MODE === 'development') {
+    path = 'http://localhost:8090'
+} else if(import.meta.env.SSR) {
+    path = 'http://127.0.0.1:6767'  // ← SSR (VPS backend)
+} else {
+    path = '/api'  // ← CLIENT (navigateur) utilise le proxy
+}
+
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
